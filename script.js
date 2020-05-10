1e77983040dcfa2959def9b166306cdd0e4f11af
maquispe <62685159+maquispe@users.noreply.github.com>
2020-05-09 20:30:07 -0700

    Adds work day scheduler

---
+++ script.js
@@ -0,0 +1,41 @@
+var today = moment().format("dddd, MMMM Do YYYY");
+$("#currentDay").text(today);
+
+var timeBlock = $(".time-block");
+timeBlock.each(function(){
+    var currentTime = moment().hours();
+    var blockTime = $(this).data('time');
+   //console.log(time);
+    var eventColor = $(this).siblings("textarea");
+    //console.log(eventColor);
+    if(blockTime < currentTime){
+        eventColor.css('background-color', 'lightgrey');
+    }
+    else if(blockTime > currentTime){
+        eventColor.css('background-color', 'limegreen');
+    }
+    else{
+        eventColor.css('background-color', 'lightcoral');
+    }
+})
+
+
+$(document).ready(function(){
+    for (i = 1; i <= 12; i++){
+        var hour = i.toString();
+        $("#" + hour).text(JSON.parse(localStorage.getItem(hour)));
+    } 
+})
+
+$(".save-btn").on("click", function(){
+	
+	var block = $(this).siblings("textarea").attr("id");
+    var entry = $(this).siblings("textarea").val();
+
+	localStorage.setItem(block, JSON.stringify(entry));
+	
+})
+
+
+    
+   
